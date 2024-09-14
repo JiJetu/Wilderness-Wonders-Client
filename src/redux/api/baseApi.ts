de24fbece5151fa457119b98f3c28c5d13afbfa4
Md Jaoadul Islam <jijetu2@gmail.com>
2024-09-14 13:53:14 +0600

    deploy

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "pokemonApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://champers-shop.vercel.app",
   }),
   tagTypes: ["product"],
   endpoints: (builder) => ({
